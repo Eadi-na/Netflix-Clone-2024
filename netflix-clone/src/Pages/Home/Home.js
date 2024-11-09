ab3ca87c3c7973a5658f7490a644cdeaf2a8c7e4
Eadi-na <eadenabebe874@gmail.com>
2024-11-09 10:14:16 -0500

    second commit

--- netflix-clone/src/Pages/Home/Home.js
+++ netflix-clone/src/Pages/Home/Home.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Header from "../../components/Header/Header";
-import Footer from "../../components/Footer/Footer";
-import Banner from "../../components/Banner/Banner";
-import RowList from "../../components/Rows/RowList/RowList";
+import Header from "../Home/Home";
+import Footer from "../../Components/Footer/Footer";
+import Banner from "../../Components/Banner/Banner";
+import RowList from "../../Components/Rows/RowList/RowList";
 
 const Home = () => {
   return (
